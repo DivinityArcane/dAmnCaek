dde66d888038a034cddd38d35c69d69b5b8ba427
Justin <eittreim.justin@live.com>
2012-08-20 22:22:42 -0700

    Initial shit. Now accepts clients (and denies them.)

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
 from Database import Database
+from Server import Server
 
 #TODO: Config
 if False == os.path.exists('Database/config'):
     print('Server is not configured.\n')
     # add more shit later?
-    test = {'address':'127.0.0.1','port':'3900','maxConnections':'50'}
-    Database.saveFile('Database/config', test)
+    config = {'address':'127.0.0.1','port':'3900','maxConnections':'50'}
+    Database.saveFile('Database/config', config)
 else:
-    test = Database.loadFile('Database/config')
-    if test is None:
+    config = Database.loadFile('Database/config')
+    if config is None:
         print('Failed to load config!\n')
     else:
         print('Wee')
-        for key in test:
-            print('{0}={1}\n'.format(key, test[key]))
+
+        serv = Server(str(config['address']),
+                      int(config['port']),
+                      int(config['maxConnections']))
+
+        serv.run()
