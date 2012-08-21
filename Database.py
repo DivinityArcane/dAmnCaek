00955e9027f7b0643830f3628a83394c7f5a1b9f
Justin <eittreim.justin@live.com>
2012-08-20 20:46:00 -0700

    Basics

---
+++ Database.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import re
+
+class Database():
+    def __init__(self):
+        {}#TODO: constructor
+        
+    def saveFile(file, data):
+        if isinstance(data, dict):
+            try:
+                fileName = re.sub(r'[^a-zA-Z0-9/]*', '', file)
+                fileData = ''
+                for key in data:
+                    fileData += '{0}={1}\0'.format(key, data[key])
+                fileHandle = open(fileName, 'w+')
+                if fileHandle:
+                    fileHandle.write(fileData)
+                    fileHandle.close()
+                else:
+                    print('Error @ Database.SaveFile: could not open file for writing: {0}'.format(fileName))
+            except:
+                print('Error @ Database.SaveFile: {0}'.format(sys.exc_info()[0]))
+        else:
+            print('Error @ Database.SaveFile: data must be a dictionary.')
+
+    def loadFile(file):
+        if os.path.exists(file):
+            try:
+                fileName = re.sub(r'[^a-zA-Z0-9/]*', '', file)
+                fileData = {}
+                handle = open(fileName, 'r')
+                if handle:
+                    tmp = handle.read().split('\0')
+                    for line in tmp:
+                        if len(line) >= 3 and '=' in line:
+                            sepPos = line.find('=')
+                            key = line[:sepPos].strip()
+                            value = line[sepPos + 1:].strip()
+                            fileData[key] = value
+                    handle.close()
+                    return fileData
+                else:
+                    print('Error @ Database.LoadFile: could not open file for reading: {0}'.format(fileName))
+            except:
+                print('Error @ Database.LoadFile: {0}'.format(sys.exc_info()[0]))
+        else:
+            print('Error @ Database.LoadFile: file does not exist: {0}'.format(file))
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import os
+from Database import Database
 
-print('I\'m a little teapot, and stuff.\n');
+#TODO: Config
+if False == os.path.exists('Database/config'):
+    print('Server is not configured.\n')
+    # add more shit later?
+    test = {'address':'127.0.0.1','port':'3900','maxConnections':'50'}
+    Database.saveFile('Database/config', test)
+else:
+    test = Database.loadFile('Database/config')
+    if test is None:
+        print('Failed to load config!\n')
+    else:
+        print('Wee')
+        for key in test:
+            print('{0}={1}\n'.format(key, test[key]))
