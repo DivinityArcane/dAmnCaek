50de9dc0aad0d0ca6f8c4d6c5b9daf0a30e7e2cf
Justin <eittreim.justin@live.com>
2012-08-20 22:23:56 -0700

    Initial shit. Now accepts clients (and denies them.)

---
+++ Client.py
@@ -0,0 +1,40 @@
+import socket
+import sys 
+import threading
+
+class Client(threading.Thread):
+
+    def __init__(self, parent, sock, clientAddr):
+        threading.Thread.__init__(self)
+        self.server = parent #we will need this ref later
+        self.sock = sock
+        self.addr = clientAddr
+        self.bufferSize = 1 #it's easier lol
+        print('Client incoming: {0}'.format(self.addr))
+
+    def run(self):
+        self.running = True
+        data = ''
+
+        while self.running:
+            buffer = self.sock.recv(self.bufferSize)
+            if buffer:
+                try:
+                    data += buffer.decode("utf-8")
+                    if data.endswith('\0'):
+                        self.process(data)
+                        data = ''
+                except:
+                    self.sock.sendall(bytes('disconnect\ne=bad data\n\0', 'utf-8'))
+                    self.sock.close()
+                    self.running = False
+            else:
+                self.sock.close()
+                self.running = False
+
+    def process(self, data):
+        #TODO: Handle packets!
+        print('Unhandled packet: {0}'.format(data.replace('\n', '\\n')))
+        self.sock.sendall(bytes('disconnect\ne=TODO: Handle packets!\n\0', 'utf-8'))
+        self.sock.close()
+        self.running = False
---
+++ Server.py
@@ -0,0 +1,36 @@
+import socket
+import sys 
+import threading 
+from Client import Client
+
+class Server():
+    def __init__(self, ip, port, maxCn):
+        self.ip = ip
+        self.port = port
+        self.maxConnections = maxCn
+        self.clients = {}
+        self.threadPool = []
+        self.clientCount = 0
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.sock.bind((ip, port))
+            self.sock.listen(self.maxConnections)
+            if self.sock:
+                print('Woot! We established the socket on {0}:{1}\n'.format(self.ip ,self.port))
+        except:
+            print('Error while setting up server: {0}\n'.format(sys.exc_info()[0]))
+            return
+
+    def run(self):
+        self.running = True
+
+        while self.running:
+            (clientSock, clientAddr) = self.sock.accept()
+            client = Client(self, clientSock, clientAddr)
+            client.run()
+            self.threadPool.append(client)
+
+        self.sock.close()
+        for thread in self.threadPool:
+            thread.join()
