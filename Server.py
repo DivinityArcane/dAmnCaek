2169c362b22bfbdb19e0df29fae30cb7e1b2347c
Orrin Fuchs <fox@fox-Satellite-C655D.(none)>
2012-08-22 17:34:48 -0500

    Added error handling Extras.

--- Server.py
+++ Server.py
@@ -2,6 +2,10 @@ import socket
 import sys 
 import threading 
 from Client import Client
+# Modified 8/22/2012
+#
+# try a more direct error handling method. for the hell of it. 
+import traceback
 
 class Server():
     def __init__(self, ip, port, maxCn):
@@ -11,27 +15,37 @@ class Server():
         self.clients = {}
         self.threadPool = []
         self.clientCount = 0
-        try:
+        try:            
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.sock.bind((ip, port))
-            self.sock.listen(self.maxConnections)
+            self.sock.listen(self.maxConnections)         
             if self.sock:
                 print('Woot! We established the socket on {0}:{1}'.format(self.ip ,self.port))
-        except:
-            print('Error while setting up server: {0}'.format(sys.exc_info()[0]))
+                #server loaded and is working. so self.running = True                
+                self.running = True
+        except:            
+            if socket.error:            
+                print('Socket Error while setting up server: {0}'.format(traceback.format_exc().splitlines()[-1]))
+            else: print('Error while setting up server:\n{0}'.format(traceback.format_exc()))            
+            #something has gone wrong, so self.running = False
+            self.running = False
             return
+            
 
     def run(self):
-        self.running = True
-
+        # if the server failed to load 
+        # it wont attempt this, which would only return another traceback. so yeah :P
         while self.running:
-            (clientSock, clientAddr) = self.sock.accept()
-            self.clientCount += 1
-            client = Client(self, clientSock, clientAddr)
-            client.run()
-            self.threadPool.append(client)
-
+            try:            
+                (clientSock, clientAddr) = self.sock.accept()
+                self.clientCount += 1
+                client = Client(self, clientSock, clientAddr)
+                client.run()
+                self.threadPool.append(client)
+            except KeyboardInterrupt:
+                print("Server stopped [Keyboard interrupt]")
+                self.running = False
         self.sock.close()
         for thread in self.threadPool:
             thread.join()
